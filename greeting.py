6c8899d2dce4306eea7da04d5737c6e88b0ccce7
Monsieur Gauso <monsieur.gauso@muss-nicht-echt-sein.de>
2026-03-18 08:57:26 +0100

    feat: Hilfsfunktion für Zufallszahl implementiert

---
+++ greeting.py
@@ -0,0 +1,8 @@
+import random
+
+def create_random_number(a=1,o=100):
+    random_number = random.randint(a, o)
+    return random_number
+
+if __name__ == '__main__':
+    print(create_random_number)
